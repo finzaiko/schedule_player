d12d3a31f682c819be07c287af81b5a62907e0a2
Arifin <arifin@citratubindo.com>
2023-05-12 15:46:12 +0700

    upd install instruction

--- install-build.sh
+++ install-build.sh
@@ -2,5 +2,5 @@
 
 IP_ADDRESS=$(hostname  -I | cut -f1 -d' ')
 echo "IP_ADDRESS=$IP_ADDRESS"
-sed -i 's,http://localhost:3000,'http://${IP_ADDRESS##}:3000',g' 'public/assets/myapp.js'
+sed -i 's,http://localhost:3000,'http://${IP_ADDRESS}:3000',g' 'public/assets/myapp.js'
 echo "Ok, setup done'
